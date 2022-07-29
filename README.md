765fa65cba4708bbcfa5fce423460000b056a6c4
Haruka08 <haruka.hashimoto08@gmail.com>
2022-07-29 20:43:51 +0100

    first commit to add files added and modified in local repository

---
+++ README.md
@@ -0,0 +1,26 @@
+# Day Planner
+Website to enter events during work hours and save the evens for future review
+
+## Links to the webpage and repository
+
+To access the website use [Day Planner](https://haruka08.github.io/Harukas-Portfolio/)
+
+To access the GitHub repository use [GitHub Repository Day Planner](https://github.com/Haruka08/Harukas-Portfolio)
+
+## Content
+
+1. At the top of the page there is an area showing the date with the current time.
+
+2. The planner consists of work hours listed on the left hand side of the list between 9am to 5pm.
+
+3. The mid section of the planner allows the user to enter information
+
+4. The right hand side button saves the use input on to the local storage.
+
+5. When the user input is save, a message appears at the top of the list confirming that the data has been saved. 
+
+6. When the page is refreshed, the information remains on the page.
+
+## Screenshot of the modified website
+
+![Completed website](./assets/screenshot.jpeg)
---
+++ assets/script.js
@@ -0,0 +1,67 @@
+// current time
+var today = moment().format('dddd, Do MMMM');
+$("#currentDay").text(today)
+
+// array to id allocated for each <input>
+var time = [
+    '#nineAm',
+    '#tenAm',
+    '#elevenAm',
+    '#twelvePm',
+    '#onePm',
+    '#twoPm',
+    '#threePm',
+    '#fourPm',
+    '#fivePm',
+];
+
+// array to id allocated for each <span>
+var workTime = [
+    '#nine',
+    '#ten',
+    '#eleven',
+    '#twelve',
+    '#one',
+    '#two',
+    '#three',
+    '#four',
+    '#five',
+];
+
+// settings dependent on time
+var now = moment().format('HH')
+
+for (var i = 0; i < workTime.length; i++){
+    var workHour = workTime[i]
+    var calendar = moment($(workHour).text(),"hh:mm").format('HH')
+
+    if (now > calendar) {
+        $("input").addClass("past");
+    } else if (now = calendar) {
+        $("input").addClass("present");
+    } else {
+        $("input").addClass("future");
+    }
+};
+
+
+// function when button is clicked
+$("button").on("click", function(event){
+    // event.preventDefault();
+    var i = $(this).val()
+    var enteredTime = time[i];
+ 
+    console.log($(enteredTime).val());
+    localStorage.setItem(i + 'hour', $(enteredTime).val());
+    $("p#alert").text("Appointment added to Local Storage");
+    setTimeout(function() { $("p#alert").hide(); }, 5000);
+
+});
+
+// To retrieve data saved on local storage when the page is refreshed
+for (var i = 0; i < time.length; i++){
+    var enteredEvent = time[i];
+    $(enteredEvent).val(localStorage.getItem(i + "hour"));
+}
+
+
